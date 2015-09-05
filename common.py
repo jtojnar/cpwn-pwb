7912c5d63260b6880c0e15a056f4fc450c79d24e
Jan Tojnar <jtojnar@gmail.com>
2015-09-05 20:48:02 +0200

    something

--- common.py
+++ common.py
@@ -1,7 +1,27 @@
 from colorama import Fore, Back, Style
-import urllib
+import urllib.request
 import os, tempfile
 import json
+import re
+
+def formatting(text):
+	text = re.sub(r'^(==+)([^= ].+)\1 *$', r'\1 \2 \1', text, flags=re.M)
+	text = re.sub(r'border="1" class="wikitable"', r'class="wikitable"', text, flags=re.M)
+	text = re.sub(r"^(!)'''(.+?)'''(?:(!!)'''(.+?)'''(?:(!!)'''(.+?)'''(?:(!!)'''(.+?)'''(?:(!!)'''(.+?)'''(?:(!!)'''(.+?)''')?)?)?)?)?", r'\1\2\3\4\5\6\7\8\9\10\11\12', text, flags=re.M)
+	text = re.sub(r'^(\|[^=\n]*)(?:([^ ])| )=(?:([^ ])| )', r'\1\2 = \3', text, flags=re.M)
+	text = re.sub(r'^(\|(colspan|rowspan)) = ', r'\1=', text, flags=re.M)
+	text = re.sub(r'== SWFs? ==\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/icons/(\d+)\.swf .+ \(icons?\)\]\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/photos/\1\.swf .+ \(photos?\)\]', r'{{Photoswf|\1}}', text)
+	text = re.sub(r'== SWFs? ==\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/icons/(\d+)\.swf .+ \(icons?\)\]\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/paper/\1\.swf .+ \(paper\)\]\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/sprites/\1\.swf .+ \(sprites?\)\]', r'{{Itemswf|\1}}', text)
+	text = re.sub(r'([^\n])(\n\[\[pt:(?!\]\]).+\]\])', r'\1\n\2', text)
+	text = re.sub(r'([^\n=])\n(==+)(.+)\2$', r'\1\n\n\2\3\2', text, flags=re.M)
+	text = re.sub(r'^\}\}([^\n])', r'}}\n\n\1', text, flags=re.M)
+	text = re.sub(r'== SWFs? ==\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/icons/(\d+)\.swf .+ \(icons?\)\]\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/paper/\1\.swf .+ \(paper\)\]\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/sprites/\1\.swf .+ \(sprites?\)\]', r'{{Itemswf|\1}}', text)
+	text = re.sub(r'== SWFs? ==\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/icons/(\d+)\.swf .+ \(icons?\)\]\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/sprites/\1\.swf .+ \(sprites?\)\]\n\* ?\[http://media1\.clubpenguin\.com/play/v2/content/global/clothing/paper/\1\.swf .+ \(paper\)\]', r'{{Itemswf|\1}}', text)
+	text = re.sub(r'^(\{\{(?:Itemswf|Photoswf|Furnitureswf)\|[^}\n]+\}\}\n)([^\n])', r'\1\n\2', text, flags=re.M)
+	text = re.sub(r'^([#:*]+)([^ #:*])', r'\1 \2', text, flags=re.M)
+	text = re.sub(r'[  ]+$', '', text, flags=re.M)
+
+	return text
 
 def edit(editor, content=''):
 	f = tempfile.NamedTemporaryFile(mode='w+')
